f0ca86bba1e8a01ee0a1d5c8547f714ff6a4649c
HavenGo <havengoitom2@gmail.com>
2025-07-17 10:38:25 +0300

    added the serializers

--- DRF/posts/views.py
+++ DRF/posts/views.py
@@ -2,7 +2,8 @@ from rest_framework.request import Request
 from rest_framework.response import Response 
 from rest_framework import status
 from rest_framework.decorators import api_view
-
+from .models import Post
+from .serializers import PostSerializer
 @api_view(["GET", "POST"])
 def homepage(request:Request):
     if request.method == "POST":
@@ -13,27 +14,19 @@ def homepage(request:Request):
     return Response(data=ans, status= status.HTTP_200_OK)
 @api_view(["GET"])
 def list_posts(request:Request):
-    return Response(data=posts, status= status.HTTP_200_OK)
+    posts = Post.objects.all()
+    
+    serializer= PostSerializer(instance=posts, many=True)
+    response={
+        "message":"posts",
+        "data": serializer.data
+    }
+    return Response(data=response, status= status.HTTP_200_OK)
 @api_view(["GET"])
 def post_detail(request:Request, post_index:int):
-    post = posts[post_index]
+    post = Post[post_index]
     if post:
         return Response(data=post, status= status.HTTP_200_OK)
     return Response(data={"error":"Post not found"}, status= status.HTTP_404_NOT_FOUND)
-posts = [
-  {
-    "id": 1,
-    "title": "Why is it difficult to learn Programming?",
-    "content": "This is to give reasons why it is hard"
-  },
-  {
-    "id": 2,
-    "title": "Learn JavaScript",
-    "content": "This is a course on JS"
-  },
-  {
-    "id": 3,
-    "title": "Python for Beginners",
-    "content": "This post covers the basics of Python programming."
-  }
-]
+
+
